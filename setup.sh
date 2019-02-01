52bbed64dd89365487dbc22d6bff9bf558141255
qw46478 <46646584+qw46478@users.noreply.github.com>
2019-02-01 10:12:57 +0200

    Add files via upload

---
+++ setup.sh
@@ -0,0 +1,48 @@
+#!/bin/bash 
+
+git clone https://github.com/qw46478/Thebast
+mkdir apk /sdcard
+pkg install unzip 
+cd Thebast
+unzip Thebast.zip
+
+clear
+sleep 2
+echo "THE BAST 10%"
+sleep 2
+clear
+echo "THE BAST 20%"
+sleep 2
+clear
+echo "THE BAST 30%"
+sleep 2
+clear
+echo "THE BAST 40%"
+sleep 2
+clear
+echo "THE BAST 50%"
+sleep 2
+clear
+echo "THE BAST 60%"
+sleep 2
+clear
+echo "THE BAST 70%"
+sleep 2
+clear
+echo "THE BAST 80%"
+sleep 2
+clear
+echo "THE BAST 90%"
+sleep 2
+clear
+echo "THE BAST 100%"
+sleep 2
+clear
+cd 
+rm -rf setupp
+cd Thebast 
+cd Thebast
+
+chmod +x +
+
+bash thebast.sh
